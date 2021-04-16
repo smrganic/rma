f2f0da82d395148f48326a0526828bb1f89c2b79
Stjepan Mrgnaić <stjepan.mrganic@gmail.com>
2021-04-16 09:40:53 +0200

    Set onClickListener to imageView

--- Task2/app/src/main/java/com/example/lv2/recycler/PersonViewHolder.kt
+++ Task2/app/src/main/java/com/example/lv2/recycler/PersonViewHolder.kt
@@ -4,10 +4,11 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.lv2.databinding.RecyclerItemBinding
+import com.example.lv2.listeners.OnSelectedPersonListener
 import com.example.lv2.model.InspiringPerson
 
 class PersonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    fun bind(person: InspiringPerson) {
+    fun bind(person: InspiringPerson, listener: OnSelectedPersonListener) {
         val itemBinding = RecyclerItemBinding.bind(itemView)
         itemBinding.tvRecyclerItemName.text = person.name
         itemBinding.tvRecyclerItemBDay.text = person.dateOfBirth
@@ -16,5 +17,6 @@ class PersonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         Glide.with(itemView.context)
             .load(person.imageLink)
             .into(itemBinding.ivRecyclerItemProfile)
+        itemBinding.ivRecyclerItemProfile.setOnClickListener { listener.onSelectedPerson(person) }
     }
 }
