88e13d28c93fa6cce4527483d0f6f7bdeef5f0d6
Stjepan Mrgnaić <stjepan.mrganic@gmail.com>
2021-05-22 00:01:53 +0200

    Added marker to current location

--- LV5_Task_2/app/src/main/java/com/example/lv5_task_2/ui/MapsActivity.kt
+++ LV5_Task_2/app/src/main/java/com/example/lv5_task_2/ui/MapsActivity.kt
@@ -1,5 +1,13 @@
 package com.example.lv5_task_2.ui
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.Manifest.permission.CAMERA
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lv5_task_2.R
@@ -10,10 +18,25 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.annotations.AfterPermissionGranted
+import java.util.jar.Manifest
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
+    private lateinit var locationManager: LocationManager
+
+    private val locationListener = LocationListener { location ->
+        val position = LatLng(location.latitude, location.longitude)
+        map.clear()
+        map.addMarker(MarkerOptions().position(position).title("Custom Marker"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(position))
+    }
+
+    companion object {
+        const val REQUEST_CODE_LOCATION_AND_CAMERA_PERMISSION = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +45,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
     }
 
     /**
@@ -40,5 +64,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        methodRequiresTwoPermission()
+    }
+
+    @SuppressLint("MissingPermission")
+    @AfterPermissionGranted(REQUEST_CODE_LOCATION_AND_CAMERA_PERMISSION)
+    private fun methodRequiresTwoPermission() {
+        if (EasyPermissions.hasPermissions(this, ACCESS_FINE_LOCATION, CAMERA)) {
+            val criteria = Criteria()
+            criteria.accuracy = Criteria.ACCURACY_FINE
+            val provider = locationManager.getBestProvider(criteria, true)
+            val minTime = 1000L
+            val minDistance = 10.0f
+            locationManager.requestLocationUpdates(provider!!, minTime, minDistance, locationListener)
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(
+                host = this,
+                rationale = "This app need location and camera.",
+                REQUEST_CODE_LOCATION_AND_CAMERA_PERMISSION,
+                ACCESS_FINE_LOCATION, CAMERA
+            )
+        }
     }
 }
